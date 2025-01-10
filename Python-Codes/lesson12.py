5812b833fc2836516f0abdf54b53bf3462b00309
Richard <ZjuRichard@outlook.com>
2025-01-10 19:39:30 +0800

    lesson 12 modified, date:2025/1/10

--- Python-Codes/lesson12.py
+++ Python-Codes/lesson12.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread(r'picture8.jpg', cv2.IMREAD_GRAYSCALE)  # 以灰度模式读取图像
+image = cv2.imread(r'picture3.jpg', cv2.IMREAD_GRAYSCALE)  # 以灰度模式读取图像
 edges = cv2.Canny(image, 150, 200)  # 应用Canny边缘检测
 
 plt.figure(figsize=(16,8))
